2403115d9ce8350d591fc4ceb809bff2b7ab52cf
gshah2020 <gaurang.r.shah@gmail.com>
2019-02-23 14:26:36 -0500

    Updates

---
+++ precache-manifest.3a6c8185939bee26ec206d4a7cb2fa18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "966b9ace7edb34183996",
+    "url": "/usrmgr_traversy/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "966b9ace7edb34183996",
+    "url": "/usrmgr_traversy/static/js/main.966b9ace.chunk.js"
+  },
+  {
+    "revision": "1b6ca76afd5d1f37cd29",
+    "url": "/usrmgr_traversy/static/js/runtime~main.1b6ca76a.js"
+  },
+  {
+    "revision": "e8dbb1b5ac0847751a75",
+    "url": "/usrmgr_traversy/static/css/2.d4815a1d.chunk.css"
+  },
+  {
+    "revision": "e8dbb1b5ac0847751a75",
+    "url": "/usrmgr_traversy/static/js/2.e8dbb1b5.chunk.js"
+  },
+  {
+    "revision": "9f4dcaf35cca18a84844e1c635164e9d",
+    "url": "/usrmgr_traversy/index.html"
+  }
+];
